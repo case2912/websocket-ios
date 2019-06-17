2b172e25a2ca0f4f06db667d58f8344b97e52c2e
case2912 <shiroyama@maia.eonet.ne.jp>
2019-06-17 20:08:57 +0900

    Initial Commit

---
+++ websocket-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  websocket-ios
+//
+//  Created by Kento Shiroyama on 2019/06/17.
+//  Copyright © 2019 Kento Shiroyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ websocket-iosTests/websocket_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  websocket_iosTests.swift
+//  websocket-iosTests
+//
+//  Created by Kento Shiroyama on 2019/06/17.
+//  Copyright © 2019 Kento Shiroyama. All rights reserved.
+//
+
+import XCTest
+@testable import websocket_ios
+
+class websocket_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
